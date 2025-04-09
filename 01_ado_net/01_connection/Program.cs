274cf5b1d134da47ed66b8f3f22cfcf78607d5d1
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-04-10 00:16:42 +0300

    add: SqlConnection

---
+++ 01_ado_net/01_connection/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connString = @"Server=.\SQLEXPRESS;Database=company_db;Trusted_Connection=True;Encrypt=False;";
+
+#region Connection
+
+using SqlConnection conn = new SqlConnection(connString);
+
+try
+{
+    conn.Open();
+    Console.WriteLine("Connection opened!");
+
+    Console.WriteLine(conn.ConnectionString);
+    Console.WriteLine(conn.State);
+    Console.WriteLine(conn.ServerVersion);
+
+    string query = @"
+        CREATE TABLE pictures(
+            id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+            title varchar(64) NOT NULL
+        );
+    ";
+
+    SqlCommand cmd = new SqlCommand(query, conn);
+    cmd.ExecuteNonQuery();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+finally
+{
+    if (conn.State == System.Data.ConnectionState.Open)
+    {
+        conn.Close();
+        Console.WriteLine("Connection closed!");
+    }
+}
+
+
+
+
+
+
+#endregion
+
+
+
